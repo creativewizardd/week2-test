07d1bb5aee0a0dc0caf50c02ea937bb5a89f4cc9
Anup panta <95905735+creativewizardd@users.noreply.github.com>
2022-03-05 10:16:28 +0545

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -1,5 +1,13 @@
-CREATE TABLE dividend
-(
-    company character varying(12),
-    fiscal_year integer
-)
+INSERT INTO dividend VALUES
+ ('AHPC',20702071),
+ ('AHPC',20712072),
+ ('AHPC',20732074),
+ ('AHPC',20762077),
+ ('CZBIL',20692070),
+ ('CZBIL',20702071),
+ ('CZBIL',20712072),
+ ('CZBIL',20732074),
+ ('GBIME',20692070),
+ ('GBIME',20702071),
+ ('GBIME',20712072),
+ ('GBIME',20732074);
